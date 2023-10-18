123ba649afe0c0a50c476fc6cc6af97051263ef5
Eduardo Andre da Silva <edugexp@gmail.com>
2023-10-17 21:12:42 -0300

    feat: make seed and first banner of home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="banner home"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
+}
