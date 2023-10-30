538cb8a46f5f9e7ea8192a9b831a0fd59a81ebdb
Eduardo André <edugexp@gmail.com>
2023-10-30 15:33:51 -0400

    feat: add category page to show products them

--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface CategoryItemProps {
@@ -8,19 +9,21 @@ interface CategoryItemProps {
 
 export function CategoryItem({ category }: CategoryItemProps) {
   return (
-    <figure className="flex flex-col">
-      <div className="bg-category-item-gradient flex h-[9.375rem] w-full items-center justify-center rounded-tl-lg rounded-tr-lg">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={400}
-          height={400}
-          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
-        />
-      </div>
-      <div className="rounded-bl-lg rounded-br-lg bg-accent p-2">
-        <p className="text-center text-sm font-semibold">{category.name}</p>
-      </div>
-    </figure>
+    <Link href={`/category/${category.slug}`}>
+      <figure className="flex flex-col">
+        <div className="flex h-[9.375rem] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={400}
+            height={400}
+            className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          />
+        </div>
+        <div className="rounded-bl-lg rounded-br-lg bg-accent p-2">
+          <p className="text-center text-sm font-semibold">{category.name}</p>
+        </div>
+      </figure>
+    </Link>
   );
 }
